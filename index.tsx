2491a93945f7ba2581399861d93fbe832c35fae9
Brian Stoner <bsstoner@gmail.com>
2022-12-16 11:57:28 -0500

    attempt to patch for ios

--- index.tsx
+++ index.tsx
@@ -44,7 +44,9 @@ export default class OTPInputView extends Component<InputProps, OTPInputViewStat
 
     componentDidMount() {
         this.copyCodeFromClipBoardOnAndroid()
-        this.bringUpKeyBoardIfNeeded()
+        setTimeout(() => {
+            this.bringUpKeyBoardIfNeeded()
+        },0)
         this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.handleKeyboardDidHide)
     }
 
